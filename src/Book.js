e42f740e45eefe4087edbd4e26a82c520107f177
santiago motta <55471374+haka-s@users.noreply.github.com>
2020-10-28 14:37:36 -0300

    Add files via upload

---
+++ src/Book.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Book = ({img,title,author}) =>{
+    const clickHandler =()=>{
+    alert('hello world');
+  };
+    const complexExample = (author) =>{
+      console.log(author);
+
+    };
+  return (
+  <article className='book' onMouseOver={()=>{
+    console.log(title)
+  }}> 
+    <img src={img} alt="" />
+    <h1 onClick={()=> console.log(title)}>{title} </h1>
+    <h4>{author}</h4>
+    <button type='button' onClick={clickHandler}>example</button>
+    <button type="button" onClick={()=>complexExample(author)}>complex</button>
+  </article>
+  );
+}
+
+export default Book
---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+ {
+  id:1,
+  img:'https://images-na.ssl-images-amazon.com/images/I/91B0nTGLBbL._AC_UL200_SR200,200_.jpg',
+  title:'5 More Sleeps ‘til Christmas',
+  author:'Jimmy Fallon',
+},
+{
+  id:2,
+  img:'https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg',
+  title:'Greenlights',
+  author:'Matthew McConaughey',
+},
+{
+  id:3,
+  img:'https://images-na.ssl-images-amazon.com/images/I/A1XyS0D1S-L._AC_UL200_SR200,200_.jpg',
+  title:'Room on the Broom',
+  author:'Julia Donaldson',
+}
+];
